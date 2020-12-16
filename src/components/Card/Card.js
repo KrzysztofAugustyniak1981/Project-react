23b99230fa4ecbd6a34430c447a98f5dd8a8479c
Krzysztof Augustyniak <augustyniak@o2.pl>
2020-12-16 17:16:38 +0100

    Add card and column

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,10 +8,11 @@ class Card extends React.Component {
     title: PropTypes.string.isRequired,
   }
   render() {
+    const {title} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
-          {this.props.title}
+          {title}
         </h3>
       </section>
     );
