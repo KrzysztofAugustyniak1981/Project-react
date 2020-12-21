98f168cdf6fc4e0bba39d403b25c3d323076fdae
Krzysztof Augustyniak <augustyniak@o2.pl>
2020-12-21 17:08:42 +0100

    name repair

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './card.scss';
+import styles from './Card.scss';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
 
